fe9ca76d06aa29e7f74faba44e318d3582c53971
Z <zijianhuang2000@gmail.com>
2020-01-16 09:19:56 +1000

    basic

---
+++ ConsoleApp0/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConsoleApp0
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World! from console");
+
+			ILogger logger;
+			IFooService fooService;
+
+			using (var serviceProvider = new ServiceCollection()// thanks to https://thecodebuzz.com/logging-in-net-core-console-application/
+				.AddSingleton<IFooService, FooService>()
+				.AddLogging(cfg =>
+				{
+					cfg.AddConsole().AddConsole(cfg=> cfg.DisableColors=true);
+				})
+				.BuildServiceProvider())
+			{
+
+				logger = serviceProvider.GetService<ILogger<Program>>();
+
+				fooService = serviceProvider.GetService<IFooService>();
+			}
+
+			logger.LogInformation("logger information");
+			logger.LogWarning("logger warning");
+
+			fooService.DoWork();
+		}
+	}
+
+	public interface IFooService
+	{
+		void DoWork();
+	}
+
+	public class FooService : IFooService
+	{
+		private readonly ILogger logger;
+
+		public FooService(ILogger<FooService> logger)
+		{
+			this.logger = logger;
+		}
+
+		public void DoWork()
+		{
+			logger.LogInformation("Doing work.");
+			logger.LogWarning("Something warning");
+			logger.LogCritical("Something critical");
+		}
+	}
+}
